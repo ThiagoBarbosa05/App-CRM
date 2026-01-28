b245529c1ebedc70eac36ad39a3ff5c685e2d43c
thiago barbosa <thiagon16@hotmail.com>
2026-01-28 14:41:42 +0000

    umbler sync comented

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import "./jobs/birthday-job-scheduler";
 import "./jobs/update-expired-events-scheduler";
-import "./jobs/umbler-sync-scheduler";
+// import "./jobs/umbler-sync-scheduler";
 import {
   initializePubSubSubscriber,
   shutdownPubSubSubscriber,
